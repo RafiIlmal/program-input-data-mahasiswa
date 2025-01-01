bf95c2897d49123840fdda5e5b6924b0c9ee1b98
RafiIlmal <achrafiilmal@gmail.com>
2025-01-01 10:55:47 +0700

    Add files via upload

---
+++ program pemanggilan nama sesuai nomor nim.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    map<string no nim nama mahasiswa>;
+    
+    
+    cout<<"masukan data berikut: "<<endl;
+    
+    cout<<"Masukan Nama: ";
+    getline(cin,nama); 
+    
+    cout<<"Masukan kelas: ";
+    getline(cin,kelas); 
+    
+    cout<<"Masukan NIM: ";
+    getline(cin,nim); 
+    
+    cout<<"Masukan Jurusan:";
+    getline (cin,jurusan); 
+
+
+    cout << endl; 
+    
+    
+    cout << "DATA MAHASISWA" << endl;
+    cout << "---------------" << endl;
+    
+    cout << "Nama     : "<<nama<<endl;
+    cout << "kelas    : "<<kelas<<endl;
+    cout << "NIM      : "<<nim<<endl;
+    cout << "Jurusan  : "<<jurusan<<endl;
+          
+          
+      }
+    
